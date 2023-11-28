1c01f25bbf78e5d7169846a16581f4124bfefe48
Mike Sanders <mikesndrs@gmail.com>
2023-11-28 14:22:30 +0100

    custom renderer

--- components/TaxilaEventsList.vue
+++ components/TaxilaEventsList.vue
@@ -9,22 +9,65 @@
       </nuxt-link>
     </div>
     <script>
-    function initTeSSWidgets() {
-      TessWidget.Events(document.getElementById('dev-tess-widget-materials-list'),
-        'SimpleList',
-        {
-          opts: {
-            enableSearch: true,
-            truncateLength: 2
-          },
-          params: {
-            q: "science OR technology OR python OR programming OR (research software)",
-            pageSize: 10,
-          },
-          baseUrl: 'https://taxila.nl'
+      class MyCustomRenderer {
+        constructor(widget, element, options) {
+          this.widget = widget;
+          this.element = element;
+          this.options = options;
         }
-      );
-    }
+
+        initialize() {
+          const title = document.createElement('h1');
+          this.element.appendChild(title);
+          this.list = document.createElement('ul');
+          this.element.appendChild(this.list);
+        }
+
+        render(errors, data, response) {
+          this.list.innerHTML = '';
+          data.data.forEach((event) => {
+            const eventElement = document.createElement('li');
+            const title_div = document.createElement('a');
+            title_div.textContent = event.attributes['title'];
+            title_div.href = event.attributes['url'];
+            eventElement.appendChild(title_div);
+            const start_div = document.createElement('div');
+            start_div.textContent = parseTime(event.attributes['start']);
+            start_div.classList.add('text-sm');
+            start_div.classList.add('text-gray-400');
+            eventElement.appendChild(start_div);
+            const venue_div = document.createElement('div');
+            venue_div.textContent = event.attributes['online'] ? 'Online' : event.attributes['venue'];
+            venue_div.classList.add('text-sm');
+            venue_div.classList.add('text-gray-400');
+            eventElement.appendChild(venue_div);
+            this.list.appendChild(eventElement);
+          });
+        }
+      }
+
+      function parseTime(datestring) {
+        const options = { month: 'long', day: 'numeric', year: 'numeric' };
+        const formattedDate = new Date(datestring).toLocaleDateString('en-US', options);
+        return formattedDate;
+      }
+
+      function initTeSSWidgets() {
+        TessWidget.Events(document.getElementById('dev-tess-widget-materials-list'),
+          MyCustomRenderer,
+          {
+            opts: {
+              enableSearch: true,
+              truncateLength: 2
+            },
+            params: {
+              q: "science OR technology OR python OR programming OR (research software)",
+              pageSize: 10,
+            },
+            baseUrl: 'https://taxila.nl'
+          }
+        );
+      }
     </script>
     <script async="" defer="" src="https://elixirtess.github.io/TeSS_widgets/js/tess-widget-standalone.js" onload="initTeSSWidgets()"></script>
   </div>
