03e948a19459ee6a1f613da692b095f79a07ca5d
Carlos Martinez <c.martinez@esciencecenter.nl>
2024-02-08 11:29:22 +0100

    Fix text on pages

--- pages/groups.vue
+++ pages/groups.vue
@@ -7,6 +7,9 @@
           This page aims to provide an overview of these groups.
           If you would like to add your own group here, please fill <a href="https://github.com/nl-rse/website/issues/new?labels=group&projects=&template=rse-group.yml">this form.</a>
         </p>
+        <p>
+          The descriptions of the groups below include a description of the group, the size of the group (in FTEs), as well as the percentage of total time spent on RSE activities on each group (RSE focus).
+        </p>
       </div>
       <h2 class="mt-6 text-3xl lg:text-4xl font-bold font-heading mt-10">
         <span class="text-primary">RSE</span> Groups in NL
@@ -25,7 +28,7 @@
             </h4>
             <p class="mb-4 text-gray-700 leading-loose" v-text="group.research_goal" />
             <span class="mr-4 font-bold">Size: {{ group.group_size }}</span>
-            <span class="mr-4 font-bold">Focus: {{ group.group_focus }}</span>
+            <span class="mr-4 font-bold">RSE Focus: {{ group.group_focus }}</span>
             <a class="mr-4 font-bold" v-if="!!group.url" :href="group.url">website</a>
             <a class="mr-4 font-bold" v-if="!!group.contact" :href="group.contact">contact</a>
           </div>
