d384513e170d62b92d13aaba72b23791dc717eaa
Ctw <ctw@ctwhome.com>
2025-04-25 19:34:28 +0200

    Add Collaps component and integrate it into the 2025 event page

---
+++ components/Collaps.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    tabindex="0"
+    class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box mt-4"
+  >
+    <div class="collapse-title text-xl font-medium">
+      {{ title }}
+    </div>
+    <div class="collapse-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add any component-specific styles here if needed */
+.collapse-title {
+  cursor: pointer;
+}
+</style>
