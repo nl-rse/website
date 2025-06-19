4808b766678f6d4db87a62a77a557ca9d1653ab2
Lieke <liekelotte@gmail.com>
2025-06-19 08:16:14 +0200

    Create Button.vue

---
+++ components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <UButton 
+    :to="href" 
+    color="black" 
+    variant="solid"
+  >
+    <slot></slot>
+  </UButton>
+</template>
+
+<script>
+export default {
+  name: 'LinkButton',
+  props: {
+    href: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
